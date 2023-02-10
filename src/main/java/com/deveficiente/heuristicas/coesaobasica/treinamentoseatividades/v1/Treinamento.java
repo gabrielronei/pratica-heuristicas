f1916dd42ff265611f9e3845f8d1c08346670ca8
Gabriel Ronei <gabriheron@hotmail.com>
2023-02-10 14:10:39 -0300

    ex(coesao): resolvendo primeiro exercicio de coesao

--- src/main/java/com/deveficiente/heuristicas/coesaobasica/treinamentoseatividades/v1/Treinamento.java
+++ src/main/java/com/deveficiente/heuristicas/coesaobasica/treinamentoseatividades/v1/Treinamento.java
@@ -1,30 +1,49 @@
 package com.deveficiente.heuristicas.coesaobasica.treinamentoseatividades.v1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Treinamento {
 
-	private String titulo;
-	private List<String> titulosAtividades = new ArrayList<>();
+    private String titulo;
+    private List<String> titulosAtividades = new ArrayList<>();
 
-	public Treinamento(String titulo) {
-		super();
-		this.titulo = titulo;
-	}
+    public Treinamento(String titulo) {
+        super();
+        this.titulo = titulo;
+    }
 
-	public static void main(String[] args) {
-		Treinamento testeDeFogo = new Treinamento("teste de fogo para você");
-		
-		/*
-		 * Primeiro passo é vocé implementar a adição de titulos de atividades
-		 * dentro do treinamento. Como você faria? 
-		 */
+    public void addTituloAtividade(String titulo) {
+        this.titulosAtividades.add(titulo);
+    }
 
-		/*
-		 * Uma vez que você implementou a adição de titulos de atividades,
-		 * é necessário que você possibilite a descoberta da posicao do titulo
-		 * da atividade dentro do treinamento. 
-		 */
-	}
+
+    public OptionalInt getPosition(String titulo) {
+        final var index = this.titulosAtividades.indexOf(titulo);
+
+        return index < 0 ? OptionalInt.empty() : OptionalInt.of(index);
+    }
+
+    public static void main(String[] args) {
+        Treinamento testeDeFogo = new Treinamento("teste de fogo para você");
+
+        /*
+         * Primeiro passo é vocé implementar a adição de titulos de atividades
+         * dentro do treinamento. Como você faria?
+         */
+
+        testeDeFogo.addTituloAtividade("Atividade 1");
+        testeDeFogo.addTituloAtividade("Atividade 2");
+        testeDeFogo.addTituloAtividade("Atividade 3");
+
+        /*
+         * Uma vez que você implementou a adição de titulos de atividades,
+         * é necessário que você possibilite a descoberta da posicao do titulo
+         * da atividade dentro do treinamento.
+         */
+
+        OptionalInt position = testeDeFogo.getPosition("Atividade 0");
+
+        System.out.println(position);
+
+    }
 }
